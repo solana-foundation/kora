3bfb1f5db6cfec9b3cdef9e4bb5158cb5ea07b85
akhil7582 <70328407+akhil7582@users.noreply.github.com>
2025-05-14 02:21:19 +0530

    feat: add transferTransactionAction endpoint

--- crates/rpc/src/server.rs
+++ crates/rpc/src/server.rs
@@ -102,5 +102,14 @@ fn build_rpc_module(rpc: KoraRpc) -> Result<RpcModule<KoraRpc>, anyhow::Error> {
         },
     );
 
+    let _ = module.register_async_method(
+        "transferTransactionAction",
+        |rpc_params, rpc_context| async move {
+            let rpc = rpc_context.as_ref();
+            let params = rpc_params.parse()?;
+            rpc.transfer_transaction(params).await.map_err(Into::into)
+        },
+    );
+
     Ok(module)
 }
