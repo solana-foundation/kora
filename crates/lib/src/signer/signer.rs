5405b1aeb33cf5422ede4857e6331fbf354af95e
pkxro <solponomarev@gmail.com>
2025-06-25 19:53:29 -0400

    fix: pkg naming for publish (#84)
    * nit: fix package naming
    
    * nit: fix pkg naming in acitons
    
    * remove: outdated pkg
    
    * nit: fix imports
    
    * nit: fix lint

--- crates/lib/src/signer/signer.rs
+++ crates/lib/src/signer/signer.rs
@@ -1,9 +1,9 @@
 use super::{solana_signer::SolanaMemorySigner, vault_signer::VaultSigner};
 use crate::error::KoraError;
-use privy_rs::PrivySigner;
+use kora_privy::PrivySigner;
+use kora_turnkey::TurnkeySigner;
 use solana_sdk::signature::Signature as SolanaSignature;
 use std::error::Error;
-use tk_rs::TurnkeySigner;
 
 #[derive(Debug, Clone)]
 pub struct Signature {
