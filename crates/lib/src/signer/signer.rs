d444c28c30f0f3e5c690267273ff2b1194132877
pkxro <solponomarev@gmail.com>
2025-06-25 19:52:10 -0400

    nit: fix lint

--- crates/lib/src/signer/signer.rs
+++ crates/lib/src/signer/signer.rs
@@ -1,9 +1,9 @@
 use super::{solana_signer::SolanaMemorySigner, vault_signer::VaultSigner};
 use crate::error::KoraError;
 use kora_privy::PrivySigner;
+use kora_turnkey::TurnkeySigner;
 use solana_sdk::signature::Signature as SolanaSignature;
 use std::error::Error;
-use kora_turnkey::TurnkeySigner;
 
 #[derive(Debug, Clone)]
 pub struct Signature {
