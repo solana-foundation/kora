4b417c94837c1079e4e9e2960adbd3e65c7ed9ec
Kapil Choudhary <144799541+Choudharyji36@users.noreply.github.com>
2025-07-18 22:55:58 +0530

    Update transfer.rs
    code add

--- crates/rpc/src/routes/transfer.rs
+++ crates/rpc/src/routes/transfer.rs
@@ -1,1 +1,17 @@
+use axum::{Json, Router, routing::post};
+use lib::actions::transfer::{TransferTransactionRequest, TransferTransactionResponse, handle_transfer_transaction};
+
+pub fn routes() -> Router {
+    Router::new().route(
+        "/transferTransaction",
+        post(transfer_transaction_handler),
+    )
+}
+
+async fn transfer_transaction_handler(
+    Json(req): Json<TransferTransactionRequest>,
+) -> Json<TransferTransactionResponse> {
+    let result = handle_transfer_transaction(req).await.unwrap();
+    Json(result)
+}
 
