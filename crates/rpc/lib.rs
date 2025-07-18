8a5bb04c068687a203ef849f0c28715b57111478
Kapil Choudhary <144799541+Choudharyji36@users.noreply.github.com>
2025-07-18 17:12:34 +0530

    Update lib.rs
    code add

--- crates/rpc/lib.rs
+++ crates/rpc/lib.rs
@@ -1,1 +1,11 @@
+pub mod routes;
 
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let routes = routes::transfer::transfer_route();
+
+    println!("Running on http://localhost:3030");
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
